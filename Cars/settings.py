e819a1220a1985394e7829a15ed160d00ec6e627
Нурсиль <n.sultanoff.2002s@gmail.com>
2024-03-04 08:13:50 +0300

    change db (sqlite --> postgres)

--- Cars/settings.py
+++ Cars/settings.py
@@ -80,11 +80,21 @@ WSGI_APPLICATION = 'Cars.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cars',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '5429',
     }
 }
 
